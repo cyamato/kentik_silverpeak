bc7d299aa82a89b7f6d4e4e174268c7dcc344c4a
cyamato <simmer006@gmail.com>
2019-02-01 07:07:53 +0000

    update

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo
-echo Testing Silver Peak Auth
-data='{"user":"KentikBad","password":"Kronos@78"}'
+data='{"user":"Kentik","password":"Kronos@78"}'
 echo "$data"
+echo
+echo Testing Silver Peak Auth
 curl -i -k -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/authentication/login
 
 echo
 echo Testing Silver Peak Auth Type
-data='{"user":"Kentik","password":"Kronos@78"}'
-echo "$data"
 curl -i -k -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/authentication/userAuthType
+
+echo
+echo Testing Silvver Peak Login Status
+curl -i -k -X GET https://msp-us-orch.core.kronos.com/authentication/loginStatus
