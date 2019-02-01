137e0a9ff622a841fae30893e3a755691c3d490d
cyamato <simmer006@gmail.com>
2019-02-01 15:52:05 +0000

    update/

--- test.sh
+++ test.sh
@@ -7,9 +7,7 @@ echo
 echo Testing Silver Peak Auth
 curl -i -k -L -H "Content-Type: application/json" -X POST -d '{"user":"kentik","password":"Kronos@78"}' $baseURL/authentication/login
 
-# echo
-# echo Testing Silver Peak Auth Type
-# curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" $baseURL/authentication/userAuthType
+sleep 2
 
 echo
 echo Testing Silvver Peak Login Status
