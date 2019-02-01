6d65a381141b3e7282af5b7f39e946a7c6880278
cyamato <simmer006@gmail.com>
2019-02-01 06:51:55 +0000

    update

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo
 echo Testing Silver Peak Auth
-$1 = '{"user":"KentikBad","password":"Kronos@97"}'
+1='{"user":"KentikBad","password":"Kronos@97"}'
 echo $1
 curl -i -k -X POST -H "Content-Type: application/json" -d "$1" https://msp-us-orch.core.kronos.com/authentication/login
