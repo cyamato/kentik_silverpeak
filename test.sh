5b69503fadfdd359be3d368cb0dc373487f13e90
cyamato <simmer006@gmail.com>
2019-02-01 07:29:45 +0000

    update/

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ data='{"user":"kentik","password":"Kronos@78"}'
 echo "$data"
 echo
 echo Testing Silver Peak Auth
-curl -i -k -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/rest/authentication/login
+curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/rest/authentication/login
 
 echo
 echo Testing Silver Peak Auth Type
-curl -i -k -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/rest/authentication/userAuthType
+curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/rest/authentication/userAuthType
 
 echo
 echo Testing Silvver Peak Login Status
-curl -i -k -X GET https://msp-us-orch.core.kronos.com/rest/authentication/loginStatus
+curl -i -k -L -X GET https://msp-us-orch.core.kronos.com/rest/authentication/loginStatus
