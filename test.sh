938271304440a6145826a728b059b94d8cc3bfb0
cyamato <simmer006@gmail.com>
2019-02-01 07:55:46 +0000

    update/

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo
-data='{"user":"kentik","password":"Kronos@78"}'
-echo "$data"
-echo
-echo Testing Silver Peak Auth
-curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/login
+# echo
+# data='{"user":"kentik","password":"Kronos@78"}'
+# echo "$data"
+# echo
+# echo Testing Silver Peak Auth
+# curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/login
 
-echo
-echo Testing Silver Peak Auth Type
-curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/userAuthType
+# echo
+# echo Testing Silver Peak Auth Type
+# curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/userAuthType
 
 echo
 echo Testing Silvver Peak Login Status
-curl -i -k -L -X GET https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/loginStatus
+curl -i -k -L -X GET https://msp-us-orch.core.kronos.com/8.1.4.9-65644/authentication/loginStatus
