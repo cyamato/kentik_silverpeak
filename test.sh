b4df249766f5f1256dc4dd6d17d0655edd2a0f4f
cyamato <simmer006@gmail.com>
2019-02-01 15:53:44 +0000

    update/

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # echo
 baseURL='https://msp-us-orch.core.kronos.com:443/gms/rest'
-echo "$data"
 
 echo
 echo Testing Silver Peak Auth
 curl -k -L -H "Content-Type: application/json" -X POST -d '{"user":"kentik","password":"Kronos@78"}' $baseURL/authentication/login
 
-sleep 2
+sleep 5
 
 echo
 echo Testing Silvver Peak Login Status
-curl -i -k -L -X GET $baseURL/authentication/loginStatus
+curl -k -L -X GET $baseURL/authentication/loginStatus
