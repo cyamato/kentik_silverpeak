1715073e44972ffffe59076ae02968500ffe21d7
cyamato <simmer006@gmail.com>
2019-02-01 15:34:14 +0000

    update/

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ echo "$data"
 
 echo
 echo Testing Silver Peak Auth
-curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/login
+curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" $baseURL/authentication/login
 
 # echo
 # echo Testing Silver Peak Auth Type
-# curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" https://msp-us-orch.core.kronos.com/8.1.4.9-65644/rest/json/authentication/userAuthType
+# curl -i -k -L -X POST -H "Content-Type: application/json" -d "$data" $baseURL/authentication/userAuthType
 
 echo
 echo Testing Silvver Peak Login Status
