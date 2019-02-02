4690d51f89f6ceb9a69bc4cd4e55619cb9e2797a
cyamato <simmer006@gmail.com>
2019-02-02 00:50:38 +0000

    update/

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ import json
 host = "https://msp-us-orch.core.kronos.com:443/gms/rest"
 session = requests.Session
 try:
+    requests.packages.urllib3.disable_warnings()
     response = session.post(host + "/authentication/login", json={"user": "Kentik", "password": "Kronos@78"}, timeout=120, verify=False)
     if response.status_code == 200:
         print("Orchestrator login success")
-        requests.packages.urllib3.disable_warnings()
+        
         response = session.get(host + '/stats/aggregate/tunnel', timeout=120, verify=False)
         print(response.json())
     else:
